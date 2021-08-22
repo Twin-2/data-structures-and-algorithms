250efa5a02a1829894edbc4df771346d9a5feb2c
Twin-2 <davidwhit818@gmail.com>
2021-08-22 16:30:05 -0700

    finished CC 5

--- README.md
+++ README.md
@@ -40,3 +40,4 @@ To run your tests
 #### Code Challenges:
 - [Class 2](array-insert-shift/README.md)
 - [Class 3](array-binary-search/README.md)
+- [Class 4](linked-list/README.md)
---
+++ linked-list/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const LinkedList = require('./lib/ll.js');
+
+let LL = new LinkedList();
+LL.append(1);
+console.log(LL)
+console.log(LL.toString())
+LL.append(2)
+console.log(LL)
+console.log(LL.toString())
