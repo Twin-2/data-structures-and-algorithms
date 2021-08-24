eb050099a2d84a4e2943bfdefb1996977054674f
Twin-2 <davidwhit818@gmail.com>
2021-08-23 20:32:08 -0700

    finished cc 6

--- linked-list/__tests__/ll.test.js
+++ linked-list/__tests__/ll.test.js
@@ -13,8 +13,10 @@ describe('Linked List Class', () => {
     it('should be able to insert a node into the linked list at the end', () => {
         let ll = new LL();
         ll.append(1);
+        ll.append(2)
         expect(ll.head.value).toBe(1)
-        expect(ll.head.next).toBe(null)
+        expect(ll.head.next.value).toBe(2)
+        expect(ll.head.next.next).toBe(null)
     })
 
     it('should have a head property that points to first node', () => {
@@ -25,12 +27,44 @@ describe('Linked List Class', () => {
         expect(ll.head).not.toEqual(head)
     })
 
-    it('should be able to insert multiple nodes', () => {
+    it('should be able to insert before a specific node', () => {
         let ll = new LL();
         ll.append(1);
         ll.append(2);
-        expect(ll.head.value).toEqual(1);
-        expect(ll.head.next.value).toEqual(2)
+        ll.insertAfter(1, 5);
+        ll.insertAfter(2, 4);
+        expect(ll.head.next.value).toEqual(5);
+        expect(ll.head.next.next.value).toEqual(2)
+        expect(ll.head.next.next.next.value).toEqual(4)
+        expect(ll.head.next.next.next.next).toEqual(null)
+
+    })
+
+    it('should be able to insert after a specific node', () => {
+        let ll = new LL();
+        ll.append(1);
+        ll.append(2);
+        ll.insertBefore(1, 5);
+        ll.insertBefore(2, 4);
+        expect(ll.toString()).toEqual('{ 5 } -> { 1 } -> { 4 } -> { 2 } -> NULL');
+
+    })
+
+    it('should be able to insert after the last node', () => {
+        let ll = new LL();
+        ll.append(1);
+        ll.append(2);
+        ll.insertAfter(2, 5);
+        expect(ll.toString()).toEqual('{ 1 } -> { 2 } -> { 5 } -> NULL');
+
+    })
+
+    it('should be able to insert before the fist node', () => {
+        let ll = new LL();
+        ll.append(1);
+        ll.append(2);
+        ll.insertBefore(1, 5);
+        expect(ll.toString()).toEqual('{ 5 } -> { 1 } -> { 2 } -> NULL');
     })
 
     it('should be return true when finding a value that exists in the linked list', () => {
@@ -54,5 +88,13 @@ describe('Linked List Class', () => {
         ll.append(2);
         ll.append(3);
         expect(ll.toString()).toBe('{ 1 } -> { 2 } -> { 3 } -> NULL')
+    })
+
+    xit('should be able to delete a specific node', () => {
+        let ll = new LL();
+        ll.append(1);
+        ll.append(2);
+        ll.delete(2);
+        expect(ll.toString()).toEqual('{ 1 } -> NULL');
     })
 })
--- linked-list/index.js
+++ linked-list/index.js
@@ -4,8 +4,9 @@ const LinkedList = require('./lib/ll.js');
 
 let LL = new LinkedList();
 LL.append(1);
-console.log(LL)
-console.log(LL.toString())
-LL.append(2)
-console.log(LL)
+LL.append(2);
+LL.append(3);
+LL.append(4);
+LL.append(5);
+LL.insertBefore(1, 'n')
 console.log(LL.toString())
