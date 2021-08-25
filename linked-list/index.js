ad2a84255d0937deaf150e1779f55f94acc5018c
Twin-2 <davidwhit818@gmail.com>
2021-08-25 13:51:39 -0700

    finished lab 7

--- linked-list/index.js
+++ linked-list/index.js
@@ -4,9 +4,6 @@ const LinkedList = require('./lib/ll.js');
 
 let LL = new LinkedList();
 LL.append(1);
-LL.append(2);
-LL.append(3);
-LL.append(4);
-LL.append(5);
-LL.insertBefore(1, 'n')
+
 console.log(LL.toString())
+console.log('nth', LL.nthFromEnd(1))
